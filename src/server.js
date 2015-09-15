2725974f75c8dcc3a3979bd021745e47b5dd9120
Luis Argumedo <huicho.argumedo@gmail.com>
2015-09-15 15:25:33 -0500

    Added event to draw graph when receiving an ID

--- src/server.js
+++ src/server.js
@@ -8,13 +8,12 @@ import client from './twitterClient';
 import Link from './models/Link';
 import api from './api';
 
-const id = '638394608746135553';
 // CocoaLoveConf
 const user_id = '94179346';
 const PORT = 8080;
 
 var app = http.createServer();
-var socket = io(app);
+var socket_io = io(app);
 
 var connected = [];
 var unconnected = {};
@@ -26,48 +25,68 @@ var links = [];
 mongoose.connect('mongodb://localhost/test');
 app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
 
+socket_io.on('connection', (socket) => { 
+  socket.on('getGraph', (id) => { 
+    reset();
 
-tweetUser(id)
-  .then(getRetweets)
-  .then((retweets) => {
-    console.log('Retweeters are:');
-    retweets.forEach((retweet) => {
-      console.log('--', retweet.user.screen_name);
-      unconnected[retweet.user.screen_name] = retweet.user;
-    });
+    console.log('Generating graph for:', id);
+    tweetUser(id)
+      .then(getRetweets)
+      .then(handleRetweeters, console.err);
+  });
+});
 
-    async.whilst(
-      () => { 
-        console.log(connected.length);
-        return connected.length !== 0; 
-      },
-      (whileCallback) => {
-        let source = connected.shift();
-        console.log('--source', source.screen_name);
-        async.forEachOfSeries(unconnected, (retweeter, key, callback) => {
+function reset() {
+  connected = [];
+  unconnected = {};
 
-          isFollower(source, retweeter).then((following) => {
-            if (following) {
-              console.log(retweeter.screen_name, '--->', source.screen_name);
-              connected.push(retweeter);
-              addToGraph(source, retweeter);
-              delete unconnected[key];
-            } else {
-              console.log(retweeter.screen_name, '-/->', source.screen_name);
-            }
-            callback();
+  nodes = [];
+  links = [];
+}
 
-          });
-        }, () => {
-          whileCallback();
-        });
-      },
-      () => {
-        console.log('Finished all users!!!');
-        socket.emit('graph', {nodes, links});
-      });
+function handleRetweeters(retweets) {
+  console.log('Retweeters are:');
+  retweets.forEach((retweet) => {
+    console.log('--', retweet.user.screen_name);
+    unconnected[retweet.user.screen_name] = retweet.user;
   });
 
+  async.whilst(
+    () => { 
+      console.log(connected.length);
+      return connected.length !== 0; 
+    },
+    (whileCallback) => {
+      let source = connected.shift();
+      console.log('--source', source.screen_name);
+      async.forEachOfSeries(unconnected, (retweeter, key, callback) => {
+
+        isFollower(source, retweeter).then((following) => {
+          if (following) {
+            console.log(retweeter.screen_name, '--->', source.screen_name);
+            connected.push(retweeter);
+            addToGraph(source, retweeter);
+            delete unconnected[key];
+          } else {
+            console.log(retweeter.screen_name, '-/->', source.screen_name);
+          }
+          callback();
+
+        });
+      }, () => {
+        whileCallback();
+      });
+    },
+    () => {
+      console.log('Adding unconnected nodes');
+      async.forEachOfSeries(unconnected, (retweeter, key, callback) => {
+        addToGraph(null, retweeter);
+        callback();
+      }, ()=> socket_io.emit('graph', {nodes, links}));
+      console.log('Finished all users!!!');
+      
+    });
+}
 function nodeIndex(user) {
   for (let i = 0; i < nodes.length; i++) {
     if (nodes[i].screen_name == user.screen_name) return i;
@@ -79,6 +98,9 @@ function addToGraph(followed, follower) {
   nodes.push({
     id: follower.id,
     screen_name: follower.screen_name,
+    description: follower.description,
+    avatar: follower.profile_image_url,
+    fill_color: follower.profile_background_color,
     followers_count: follower.followers_count
   });
   if (followed) {
@@ -106,11 +128,13 @@ function tweetUser(id) {
   var deferred = Q.defer();
   client.get('statuses/show',{id}, (err, tweet) => {
     if (err) {
+      console.log(err);
       deferred.reject(err);
+    } else {
+      console.log('Tweet user is: ', tweet.user.screen_name);
+      addToGraph(null, tweet.user);
+      connected.push(tweet.user);
     }
-    console.log('Tweet user is: ', tweet.user.screen_name);
-    addToGraph(null, tweet.user);
-    connected.push(tweet.user);
 
     deferred.resolve(tweet);
   });
